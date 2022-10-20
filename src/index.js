fc084b0a81660929d5933372235bfa39153496d2
Januha88 <113813482+Januha88@users.noreply.github.com>
2022-10-20 14:24:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+const btnEncode = document.getElementById('encode');
+
+
+btnEncode.addEventListener('click', ()=>{
+    const textarea = document.getElementById('message');
+    const message = textarea.value;
+    const offset = document.getElementById('offset').value;
+   const valorCodificado =  cipher.encode(offset, message);
+   console.log(valorCodificado);
+   textarea.value = valorCodificado; 
+})
+
+const btnDecode = document.getElementById('decode');
+
+btnDecode.addEventListener('click', ()=>{
+    const textarea = document.getElementById('message');
+    const message = textarea.value;
+    const offset = document.getElementById('offset').value;
+    const valorDecodificado = cipher.decode(offset, message); 
+    console.log(valorDecodificado);
+    textarea.value = valorDecodificado;
+})
+
+
+
+
+
